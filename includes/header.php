33d52128df1361e6ad0213dad581d3f6a863c1b9
jackhr <jc2o@mac.com>
2023-11-10 02:57:32 -0500

    Implement villa dropdown menu.

--- includes/header.php
+++ includes/header.php
@@ -13,6 +13,7 @@
 	<link type="text/css" rel="stylesheet" href="style.css">
 	<link type="text/css" rel="stylesheet" href="js/datetimepicker/jquery.datetimepicker.css">
 	<link type="text/css" rel="stylesheet" href="js/validity/jquery.validity.css">
+    <script src="js/jquery-1.8.2.min.js"></script>
 </head>
 <body id="<?php echo $page ?>-page">
 
@@ -25,11 +26,26 @@
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-palmtree"><path d="M13 8c0-2.76-2.46-5-5.5-5S2 5.24 2 8h2l1-1 1 1h4"/><path d="M13 7.14A5.82 5.82 0 0 1 16.5 6c3.04 0 5.5 2.24 5.5 5h-3l-1-1-1 1h-3"/><path d="M5.89 9.71c-2.15 2.15-2.3 5.47-.35 7.43l4.24-4.25.7-.7.71-.71 2.12-2.12c-1.95-1.96-5.27-1.8-7.42.35z"/><path d="M11 15.5c.5 2.5-.17 4.5-1 6.5h4c2-5.5-.5-12-1-14"/></svg>
             </a>
 			<nav>
-				><a class="<?php if($page=='index') echo 'current' ?>" href="index.php"><span>Villas</span></a
-				><a class="<?php if($page=='information') echo 'current' ?>" href="general-info.php"><span>General Inforamtion</span></a
-				><a class="<?php if($page=='cars') echo 'current' ?>" href="car-rentals"><span>Car Rentals<span></a
-				><a class="<?php if($page=='contact') echo 'current' ?>" href="contact.php"><span>Contact<span></a>
+                <div class="custom-dropdown">
+                    <span id="villa-toggle" class="<?php if($page=='index') echo 'current' ?>">Villas</span>
+                    <div class="dropdown-content hidden">
+                        <span class="dropdown-item">Villa Ordnance</span>
+                        <span class="dropdown-item">Litle Rock Cottage</span>
+                        <span class="dropdown-item">Goat Rock Studio</span>
+                        <span class="dropdown-item">Goat Hill Studio</span>
+                        <span class="dropdown-item">Casa Chiesa</span>
+                    </div>
+                </div>
+				<a class="<?php if($page=='information') echo 'current' ?>" href="general-info.php"><span>General Inforamtion</span></a>
+                <a class="<?php if($page=='cars') echo 'current' ?>" href="car-rentals"><span>Car Rentals<span></a>
+                <a class="<?php if($page=='contact') echo 'current' ?>" href="contact.php"><span>Contact<span></a>
 			</nav>
 			<a id="mobile-menu"><img src="images/menu.png" width="40" height="28" /></a>
 		</div>
 	</header>
+
+    <script>
+
+        $(document).on('click', e => $('.dropdown-content').toggleClass('hidden', !$('.custom-dropdown').has(e.target).length));
+
+    </script>
