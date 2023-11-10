5de97e698ddf022a3e5f57d304655a9a519fe004
jackhr <jc2o@mac.com>
2023-11-10 13:53:26 -0500

    Update intro and add chevron to scroll down button

--- index.php
+++ index.php
@@ -3,15 +3,18 @@ $title = "title";
 $page = "index";
 include_once 'includes/header.php'
 ?>
-    <div id="intro">
-        <div>
-            <p>Discover the charm of Antigua's southern hideaways with "Titi Vacation Homes," your gateway to an authentic Caribbean experience. Our collection features five cozy villas, primarily nestled in the laid-back ambiance of English Harbour.</p>
-        </div>
-    </div>
     <div class="slideshow-container">
         <div class="slide active" style="background-image: url('http://www.villaordnance.com/images/slideshows/homepage/image-villa-aerial-30171.jpg');"></div>
         <div class="slide" style="background-image: url('http://www.villaordnance.com/images/galleries/villa-ordnance/TITI-VillaOrd-PoolToSea-1903JMR3999.jpg');"></div>
         <div class="slide" style="background-image: url('http://www.villaordnance.com/images/galleries/villa-ordnance/TITI-VillaOrd-DiningExt-1903JMR3712.jpg');"></div>
+    </div>
+    <div id="intro">
+        <div>
+            <p>Discover the charm of Antigua's southern hideaways with "Titi Vacation Homes," your gateway to an authentic Caribbean experience. Our collection features five cozy villas, nestled in the laid-back ambiance of Falmouth, Picadilly, and English Harbour.</p>
+        </div>
+        <div id="intro-chevron">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-down"><path d="m6 9 6 6 6-6"/></svg>
+        </div>
     </div>
     <script>
         let currentSlide = 0;
