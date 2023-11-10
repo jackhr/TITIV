9748d1604f2826c2c447387f90f1120a46123813
jackhr <jc2o@mac.com>
2023-11-10 03:21:32 -0500

    Add welcome message and main pic to homepage

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+$title = "title";
+$page = "index";
+include_once 'includes/header.php'
+?>
+    <div id="intro">
+        <h1>Welcome to Titi Vacation Rentals</h1>
+        <p>Thank you for visiting our website.</p>
+        <div class="overlay"></div>
+    </div>
+<?php include_once 'includes/footer.php' ?>
