02e8cb82d0155a80366b43b761889679a0850aac
jackhr <jc2o@mac.com>
2023-11-15 11:38:41 -0500

    Add MySQL connection script and extract credentials to env.php

---
+++ includes/connection.php
@@ -0,0 +1,12 @@
+<?php
+
+include_once 'env.php';
+
+$con = mysqli_connect( $hostname, $username, $password, $database );
+
+if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    exit();
+}
+
+?>
