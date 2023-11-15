c8a7a88331f4ea63ca73f27e17b896fe7350318d
jackhr <jc2o@mac.com>
2023-11-15 11:46:23 -0500

    Add show page for villas with dynamic content

---
+++ show.php
@@ -0,0 +1,14 @@
+<?php
+$villa = $_GET['villa'];
+$title = "Titi Vacation Homes - $villa";
+$page = "show";
+include_once 'includes/header.php';
+
+$villa_query = "SELECT * FROM villas WHERE name = '$villa';";
+$result = mysqli_query($con, $villa_query);
+$villa = mysqli_fetch_assoc($result);
+?>
+
+<h1><?php echo $villa['name']; ?></h1>
+
+<?php include_once 'includes/footer.php' ?>
