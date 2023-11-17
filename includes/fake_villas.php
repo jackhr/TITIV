17a75fcc529cc3a45ef79a248056264257f49c34
jackhr <jc2o@mac.com>
2023-11-17 13:32:50 -0500

    Add address and address link to villa details

--- includes/fake_villas.php
+++ includes/fake_villas.php
@@ -7,7 +7,9 @@ $villas = [
         'beds' => 5,
         'baths' => 4,
         'booking_dot_com_link' => 'https://www.booking.com/hotel/ag/villa-ordnance.html',
-        'air_bnb_link' => 'https://www.airbnb.com/rooms/plus/20392292'
+        'air_bnb_link' => 'https://www.airbnb.com/rooms/plus/20392292',
+        'address' => "Nelson's Dockyard, St. Paul, Antigua & Barbuda",
+        'address_link' => 'https://www.google.com/maps/place/Villa+Ordnance/@17.0122616,-61.7670372,18z/data=!4m9!3m8!1s0x8c12eb4233fdcaed:0xe647b8550fe6d978!5m2!4m1!1i2!8m2!3d17.0123163!4d-61.7656349!16s%2Fg%2F11t80h0r5x?entry=ttu',
     ],
     'Casa Chiesa' => [
         'name' => 'Casa Chiesa',
@@ -16,7 +18,9 @@ $villas = [
         'beds' => 3,
         'baths' => 4,
         'booking_dot_com_link' => 'https://www.booking.com/hotel/ag/casa-chiesa.html',
-        'air_bnb_link' => 'https://www.airbnb.com/rooms/plus/20392292'
+        'air_bnb_link' => 'https://www.airbnb.com/rooms/plus/20392292',
+        'address' => "Falmouth, St. Paul, Antigua & Barbuda",
+        'address_link' => 'https://maps.app.goo.gl/4nyHkHmL2ZX6yygf8'
     ],
     'Little Rock Cottage' => [
         'name' => 'Little Rock Cottage',
@@ -25,7 +29,9 @@ $villas = [
         'beds' => 2,
         'baths' => 1,
         'booking_dot_com_link' => 'https://www.booking.com/hotel/ag/little-rock-cottage.html',
-        'air_bnb_link' => 'https://www.airbnb.com/rooms/plus/20392292'
+        'air_bnb_link' => 'https://www.airbnb.com/rooms/plus/20392292',
+        'address' => "Picadilly, St. Paul, Antigua & Barbuda",
+        'address_link' => 'https://www.google.com/maps/place/Little+Rock+Cottage/@17.0210881,-61.7631826,67m/data=!3m1!1e3!4m9!3m8!1s0x8c12eb1ac1758b55:0x72a3ec35a3ee6cc4!5m2!4m1!1i2!8m2!3d17.0210637!4d-61.7630725!16s%2Fg%2F11rn2bvd_8?entry=ttu'
     ],
     'Goat Hill Studio' => [
         'name' => 'Goat Hill Studio',
@@ -34,7 +40,9 @@ $villas = [
         'beds' => 1,
         'baths' => 1,
         'booking_dot_com_link' => 'https://www.booking.com/hotel/ag/goat-hill-studio.html',
-        'air_bnb_link' => 'https://www.airbnb.com/rooms/plus/20392292'
+        'air_bnb_link' => 'https://www.airbnb.com/rooms/plus/20392292',
+        'address' => "Picadilly, St. Paul, Antigua & Barbuda",
+        'address_link' => 'https://www.google.com/maps/place/Goat+cottage/@17.0211067,-61.7631886,67m/data=!3m1!1e3!4m6!3m5!1s0x8c12eb7276e6c287:0xb30472c6205b78cc!8m2!3d17.0212166!4d-61.7631333!16s%2Fg%2F11swfs0knn?entry=ttu'
     ],
     'Goat Rock Studio' => [
         'name' => 'Goat Rock Studio',
@@ -43,7 +51,9 @@ $villas = [
         'beds' => 1,
         'baths' => 1,
         'booking_dot_com_link' => 'https://www.booking.com/hotel/ag/goat-rock-studio.html',
-        'air_bnb_link' => 'https://www.airbnb.com/rooms/plus/20392292'
+        'air_bnb_link' => 'https://www.airbnb.com/rooms/plus/20392292',
+        'address' => "Picadilly, St. Paul, Antigua & Barbuda",
+        'address_link' => 'https://www.google.com/maps/place/Goat+cottage/@17.0210881,-61.7631826,67m/data=!3m1!1e3!4m6!3m5!1s0x8c12eb7276e6c287:0xb30472c6205b78cc!8m2!3d17.0212166!4d-61.7631333!16s%2Fg%2F11swfs0knn?entry=ttu'
     ]
 ];
 ?>
