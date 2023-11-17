59ae89dc4714632d53ef7e15698f4c72cb811492
jackhr <jc2o@mac.com>
2023-11-17 13:45:39 -0500

    Add villa details section to show.php and update styling in style.css

--- show.php
+++ show.php
@@ -53,7 +53,19 @@ foreach($villa_directory_files as $idx => $file) {
             </svg>
         </div>
     </div>
-    <h1><?php echo $villa['name']; ?></h1>
+
+    <main id="details">
+        <div class="header">
+            <h1><?php echo $villa['name']; ?></h1>
+            <div class="location-container">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-map-pin">
+                    <path d="M20 10c0 6-8 12-8 12s-8-6-8-12a8 8 0 0 1 16 0Z"/>
+                    <circle cx="12" cy="10" r="3"/>
+                </svg>
+                <a href="<?php echo $villa['address_link']; ?>" target="_blank"><?php echo $villa['address']; ?></a>
+            </div>
+        </div>
+    </main>
 
 
     <script>
