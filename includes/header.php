23f748f01a8fc6030f8ec907eff9a2b438feb190
jackhr <jc2o@mac.com>
2023-12-01 09:14:30 -0500

    Improve styling and structure

--- includes/header.php
+++ includes/header.php
@@ -22,32 +22,45 @@
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-home"><path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/><polyline points="9 22 9 12 15 12 15 22"/></svg>
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-palmtree"><path d="M13 8c0-2.76-2.46-5-5.5-5S2 5.24 2 8h2l1-1 1 1h4"/><path d="M13 7.14A5.82 5.82 0 0 1 16.5 6c3.04 0 5.5 2.24 5.5 5h-3l-1-1-1 1h-3"/><path d="M5.89 9.71c-2.15 2.15-2.3 5.47-.35 7.43l4.24-4.25.7-.7.71-.71 2.12-2.12c-1.95-1.96-5.27-1.8-7.42.35z"/><path d="M11 15.5c.5 2.5-.17 4.5-1 6.5h4c2-5.5-.5-12-1-14"/></svg>
             </a>
-			<nav>
-                <div class="custom-dropdown">
-                    <span class="villa-toggle">Villas</span>
-                    <div class="dropdown-content hidden">
-                        <?php if ($prod) {
-                            $villas_query = "SELECT name FROM villas;";
-                            $result = mysqli_query($con, $villas_query);
-                            $villas = [];
-                            
-                            while($v = mysqli_fetch_assoc($result)) $villas[] = $v;
+            <?php
+            for($i = 0; $i < 2; $i++) {
+                $id_insert = $i == 1 ? 'id="mobile-nav"' : '';
+                ?>
+                <nav <?php echo $id_insert; ?>>
+                    <?php if ($i == 1) { ?>
+                        <div id="nav-close">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-x">
+                                <path d="M18 6 6 18"/>
+                                <path d="m6 6 12 12"/>
+                            </svg>
+                        </div>
+                    <?php } ?>
+                    <div class="custom-dropdown">
+                        <span class="villa-toggle">Villas</span>
+                        <div class="dropdown-content hidden">
+                            <?php if ($prod) {
+                                $villas_query = "SELECT name FROM villas;";
+                                $result = mysqli_query($con, $villas_query);
+                                $villas = [];
+                                
+                                while($v = mysqli_fetch_assoc($result)) $villas[] = $v;
 
-                            foreach($villas as $v) echo "<a class='dropdown-item' href='show.php?villa={$v['name']}'>{$v['name']}</a>";
-                        } else { ?>
-                            <a class='dropdown-item' href='show.php?villa=Villa Ordnance'>Villa Ordnance</a>
-                            <a class='dropdown-item' href='show.php?villa=Casa Chiesa'>Casa Chiesa</a>
-                            <a class='dropdown-item' href='show.php?villa=Little Rock Cottage'>Little Rock Cottage</a>
-                            <a class='dropdown-item' href='show.php?villa=Goat Hill Studio'>Goat Hill Studio</a>
-                            <a class='dropdown-item' href='show.php?villa=Goat Rock Studio'>Goat Rock Studio</a>
-                        <?php } ?>
+                                foreach($villas as $v) echo "<a class='dropdown-item' href='show.php?villa={$v['name']}'>{$v['name']}</a>";
+                            } else { ?>
+                                <a class='dropdown-item' href='show.php?villa=Villa Ordnance'>Villa Ordnance</a>
+                                <a class='dropdown-item' href='show.php?villa=Casa Chiesa'>Casa Chiesa</a>
+                                <a class='dropdown-item' href='show.php?villa=Little Rock Cottage'>Little Rock Cottage</a>
+                                <a class='dropdown-item' href='show.php?villa=Goat Hill Studio'>Goat Hill Studio</a>
+                                <a class='dropdown-item' href='show.php?villa=Goat Rock Studio'>Goat Rock Studio</a>
+                            <?php } ?>
+                        </div>
                     </div>
-                </div>
-				<a class="<?php if($page=='information') echo 'current' ?>" href="faq.php"><span>Info & FAQ</span></a>
-                <a target="_blank" href="http://www.titirentacarantigua.com/"><span>Car Rentals<span></a>
-                <a class="<?php if($page=='contact') echo 'current' ?>" href="contact.php"><span>Contact<span></a>
-			</nav>
-			<a id="mobile-menu"><img src="images/menu.png" width="40" height="28" /></a>
+                    <a class="<?php if($page=='information') echo 'current' ?>" href="faq.php"><span>Info & FAQ</span></a>
+                    <a target="_blank" href="http://www.titirentacarantigua.com/"><span>Car Rentals<span></a>
+                    <a class="<?php if($page=='contact') echo 'current' ?>" href="contact.php"><span>Contact<span></a>
+                </nav>
+            <?php } ?>
+			<a id="mobile-menu"><img src="assets/icons/menu.png" width="40" height="28" /></a>
 		</div>
 	</header>
 
@@ -65,5 +78,15 @@
                 $('.dropdown-content').addClass('hidden');
             }
         });
+
+        $('#mobile-menu').on('click', function(){
+            $('header nav').addClass('showing');
+            $('html').addClass('viewing');
+        });
+
+        $("#nav-close").on('click', function() {
+            $('header nav').removeClass('showing');
+            $('html').removeClass('viewing');
+        })
         
     </script>
--- show.php
+++ show.php
@@ -101,11 +101,13 @@ $link = $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
         <div class="header">
             <h1><?php echo $villa['name']; ?></h1>
             <div class="sub-header-container">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-map-pin">
-                    <path d="M20 10c0 6-8 12-8 12s-8-6-8-12a8 8 0 0 1 16 0Z"/>
-                    <circle cx="12" cy="10" r="3"/>
-                </svg>
-                <a href="<?php echo $villa['address_link']; ?>" target="_blank"><?php echo $villa['address']; ?></a>
+                <div class="villa-icon-pair">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-map-pin">
+                        <path d="M20 10c0 6-8 12-8 12s-8-6-8-12a8 8 0 0 1 16 0Z"/>
+                        <circle cx="12" cy="10" r="3"/>
+                    </svg>
+                    <a href="<?php echo $villa['address_link']; ?>" target="_blank"><?php echo $villa['address']; ?></a>
+                </div>
                 <span>•</span>
                 <div class="villa-icon-pair share" onclick="$('.share.modal').addClass('show');">
                     <?php echo $svg_lookup['share']; ?>
